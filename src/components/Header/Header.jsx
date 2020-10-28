b0b3fca06b527089a3c833418523531783334ba3
Daria0109 <da6ulya888@gmail.com>
2020-10-28 20:06:43 +0300

    Loginization (auth/me)(less.61)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import classes from './Header.module.css';
+import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
-    return (
-        <header className={classes.header}>
-            <img className={classes.header_img} src="https://www.freelogodesign.org/Content/img/logo-samples/flooop.png" alt="Logo"/>
-        </header>
-    )
+const Header = (props) => {
+  debugger
+  return (
+    <header className={s.header}>
+      <div className={s.logo_block}>
+        <img className={s.logo} src="https://www.freelogodesign.org/Content/img/logo-samples/flooop.png" alt="Logo"/>
+      </div>
+      <div className={s.login_block}>
+        {props.isAuth ? props.login : <NavLink to='/login'>LogIn</NavLink>}
+      </div>
+    </header>
+  )
 }
 export default Header;
