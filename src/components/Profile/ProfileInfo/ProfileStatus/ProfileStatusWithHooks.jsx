d788eb25349cf4eccd44074db4757982d884546b
Daria0109 <da6ulya888@gmail.com>
2020-12-02 12:14:26 +0300

    84. UseState hook (in ProfileStatus)

---
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+const ProfileStatusWithHooks = (props) => {
+const[editMode, setEditMode] = useState(false);
+const[status, setStatus] = useState(props.status);
+
+const updateStatus = () => {
+    props.updateStatus(status);
+    setEditMode(false)
+}
+  return <>
+    {!editMode &&
+    <div>
+      <span onDoubleClick={() => setEditMode(true)}>{props.status || "No status..."}</span>
+    </div>}
+    {editMode &&
+    <div>
+      <input autoFocus
+             value={status}
+             onChange={(e) => setStatus(e.currentTarget.value)}
+             onBlur={updateStatus}/>
+    </div>}
+  </>
+}
+
+export default ProfileStatusWithHooks;
